a3a7a024437d65655ba2db9453fb15aad7cca217
MRdima98 <dimafrunza69@gmail.com>
2022-08-24 08:00:39 +0200

    Ricerca perfezionata

--- DumitruFrunza135801/home/views.py
+++ DumitruFrunza135801/home/views.py
@@ -20,17 +20,14 @@ def home(request):
 
 def search(request, city, start, end, people):
     hotels = Hotel.objects.filter(city = city) 
+    print(hotels)
     for hotel in hotels:
         rooms = Rooms.objects.filter(hotel = hotel, 
         people=people).select_related('hotel')
+    print(rooms)
     for room in rooms: 
-        cost = Cost.objects.filter(room=room, begin_date=start, end_date=end).select_related('room')
-
-    print(room,cost,hotels)
+        cost = Cost.objects.filter(room=room, begin_date__gt=start, end_date__lt=end).select_related('room')
     context = {
-        'rooms' : rooms,
-        'results' : range(len(hotels)),
-        'hotel' : hotels,
         'costs' : cost
     }
     return render(request, "search.html", context)

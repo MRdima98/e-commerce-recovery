fcb9f346b4b0d071682e18406187cc564c07b8a0
MRdima98 <dimafrunza69@gmail.com>
2022-08-24 07:38:35 +0200

    Search query for 1 hotel done

--- DumitruFrunza135801/home/views.py
+++ DumitruFrunza135801/home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .froms import SearchFrom
 # Create your views here.
 
-from hotel.models import Hotel, Rooms
+from hotel.models import Hotel, Rooms, Cost
 
 def home(request):
     form = SearchFrom()
@@ -19,12 +19,18 @@ def home(request):
     return render(request, "index.html", context)
 
 def search(request, city, start, end, people):
-    hotel = Hotel.objects.filter(city = city) 
-    rooms = Rooms.objects.filter(hotel = hotel[0])
-    print(hotel)
+    hotels = Hotel.objects.filter(city = city) 
+    for hotel in hotels:
+        rooms = Rooms.objects.filter(hotel = hotel, 
+        people=people).select_related('hotel')
+    for room in rooms: 
+        cost = Cost.objects.filter(room=room, begin_date=start, end_date=end).select_related('room')
+
+    print(room,cost,hotels)
     context = {
         'rooms' : rooms,
-        'results' : range(len(hotel)),
-        'hotel' : hotel 
+        'results' : range(len(hotels)),
+        'hotel' : hotels,
+        'costs' : cost
     }
     return render(request, "search.html", context)

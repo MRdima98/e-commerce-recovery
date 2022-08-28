db9f36ab624a6fb401ed44c603edef069d363a9c
MRdima98 <dimafrunza69@gmail.com>
2022-08-28 16:12:55 +0200

    Login done

--- DumitruFrunza135801/login/views.py
+++ DumitruFrunza135801/login/views.py
@@ -1,6 +1,10 @@
+from calendar import firstweekday
+from email import message
 from multiprocessing import context
 from django.shortcuts import render
 from .forms import RegisterForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def log_in(request):
     return render(request, 'log_in.html', {})
@@ -8,7 +12,18 @@ def log_in(request):
 def register(request):
     register_form = RegisterForm(request.POST or None)
     if register_form.is_valid():
-        pass
+        if register_form.cleaned_data['passwd'] == register_form.cleaned_data['confirm_passwd']:
+            User.objects.create_user(
+                username = register_form.cleaned_data['username'],
+                first_name = register_form.cleaned_data['name'],
+                last_name = register_form.cleaned_data['surname'],
+                email = register_form.cleaned_data['email'],
+                password = register_form.cleaned_data['passwd'],
+            )
+        else: 
+            print('nope')
+            messages.error(request, 'Le password non coincidono')
+    
     context = { 'form' : register_form }
     return render(request, 'register.html', context)
 

17f69b4fe8290986f9764243f61bee148ffc766d
dima <dimafrunza69@gmail.com>
2022-09-04 06:05:51 +0200

    tolta la mia password gmail

--- DumitruFrunza135801/DumitruFrunza135801/settings.py
+++ DumitruFrunza135801/DumitruFrunza135801/settings.py
@@ -141,7 +141,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'dimafrunza69@gmail.com'
-EMAIL_HOST_PASSWORD = 'joxctgdfeeetdnrf'
+EMAIL_HOST_PASSWORD = ''
 
 CELERY_TIMEZONE = "Europe/Rome"
 CELERY_BROKER_URL = 'amqp://guest:guest@localhost'
